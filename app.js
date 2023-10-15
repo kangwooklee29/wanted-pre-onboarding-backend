4b6ba47cd99f67143a650b3b3486c9a28cffad67
kangwooklee29 <kangwooklee29@gmail.com>
2023-10-15 12:38:39 +0000

    Initial commit for #13

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Company, JobAd } = require('./models'); // Sequelize model import
+const { Company, JobAd, User, UserJobAd } = require('./models'); // Sequelize model import
 
 const app = express();
 
@@ -127,6 +127,35 @@ app.get('/jobad/:id', async (req, res) => {
         };
 
         res.send(result);
+
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+});
+
+app.post('/user-job-ad', async (req, res) => {
+    try {
+        const { userId, jobAdId } = req.body;
+
+        // User 찾기 또는 생성하기
+        const [user, created] = await User.findOrCreate({
+            where: { id: userId }
+        });
+
+        // JobAd가 존재하는지 확인
+        const jobAd = await JobAd.findByPk(jobAdId);
+
+        if (!jobAd) {
+            return res.status(400).json({ message: 'JobAd not found' });
+        }
+
+        const newUserJobAd = await UserJobAd.create({
+            userId: user.id,
+            jobAdId
+        });
+
+        // Created a new job application info
+        res.status(201).json(newUserJobAd);
 
     } catch (error) {
         res.status(500).send({ message: error.message });
